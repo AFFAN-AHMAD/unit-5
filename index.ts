e3cdaffa9e7b5a85b546a86211d2b61faabf0f80
AFFAN-AHMAD <96197969+AFFAN-AHMAD@users.noreply.github.com>
2022-05-17 08:54:42 +0530

    assignment typescript

---
+++ index.ts
@@ -0,0 +1,15 @@
+
+
+type User = {
+    id: string;
+    name: string;
+    age: number;
+    salary?: number;
+    
+};
+type Teacher = {
+    id: string;
+    name: string;
+    sub: string;
+    name_of_students: string;
+}
